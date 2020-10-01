a100f7c110601c3b6e9ed9a0f5c08b2441d252bc
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2020-10-01 19:10:22 +0200

    Added season, maps and modes commands
    Added season, maps and modes commands to the Discord bot.

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,4 +1,6 @@
 from RiotAPI import RiotAPI #used to call to riot api
+from LoLConstants import LoLConstants
+from DataDragon import DataDragon
 from discord.ext.commands import Bot
 
 
@@ -25,5 +27,27 @@ async def riot(ctx, playername):
     await ctx.send(text)
 
 
+@bot.command()
+async def season(ctx):
+    curr_season = LoLConstants.seasons()[-1]["season"]
+    text = f"The current League of Legends season is: {curr_season}"
+    await ctx.send(text)
+
+@bot.command()
+async def maps(ctx):
+    maps_info = LoLConstants.maps()
+    text = "**League of legends has the following maps:**\n\n"
+    for i, map_info in enumerate(maps_info, start=1):
+        text += "{}. **{}** : {}\n".format(i, map_info["mapName"], map_info["notes"])
+    await ctx.send(text)
+
+
+@bot.command()
+async def modes(ctx):
+    modes_info = LoLConstants.gameModes()
+    text = "**League of legends has the following game modes:**\n\n"
+    for i, mode_info in enumerate(modes_info , start=1):
+        text += "{}. **{}** : {}\n".format(i, mode_info["gameMode"], mode_info["description"])
+    await ctx.send(text)
 
 bot.run("KEY")
